2096f8d2528d915b067c381abd73379c84cdf42b
Pâmella Oliveira Endlich <pamellaend@gmail.com>
2021-08-25 17:23:41 -0300

    test

---
+++ app/src/androidTest/java/com/pamella/moviesapp/HomeActivity.kt
@@ -0,0 +1,28 @@
+package com.pamella.moviesapp
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.pamella.moviesapp.ui.activitys.HomeActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HomeActivity {
+
+    @get:Rule
+    val homeActivity = ActivityScenarioRule(HomeActivity::class.java)
+
+
+    @Test
+     fun search_test(){
+        onView(withId(R.id.searchMovieBox)).perform(click())
+        onView(withId(R.id.searchMovieBox)).perform(typeText("Frozen"), pressKey(66))
+        onView(withId(R.id.backHomeButton)).perform(click())
+    }
+
+
+}
